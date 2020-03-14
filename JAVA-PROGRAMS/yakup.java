642a22b751dd7c6e83a9c1c145d4f26261ed181d
vivolric <62187112+vivolric@users.noreply.github.com>
2020-03-14 14:32:41 -0500

    Create yakup.java

---
+++ JAVA-PROGRAMS/yakup.java
@@ -0,0 +1,26 @@
+public class asdf {
+    public static void main(String[]args)
+    {
+        System.out.print("Bu Is boyle olmayacak");
+        System.out.print("Bu Is boyle olmayacak");
+        System.out.println("Bu Is boyle olmayacak");
+        System.out.println("Bu Is boyle olmayacak");
+        System.out.print("Bu Is boyle olmayacak");
+        System.out.print("Bu Is boyle olmayacak");
+        String a = "Hakki bulut";
+        int b = 44;
+        double c = 4.2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println(a+ " asdf" + " " + b );
+        System.out.println(c);
+        double d = b + c;
+        System.out.println( "b ile c nin toplami " + d);
+        if (b<40) System.out.println("kirktan kucuk");
+        else System.out.println("degil");
+
+
+
+    }
+}
