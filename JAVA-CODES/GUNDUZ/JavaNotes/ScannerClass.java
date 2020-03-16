e208b033d7731a3163b22d8565f3654fc82f445c
RIDVAN GUNDUZ <58498991+rgunduz17@users.noreply.github.com>
2020-03-16 13:36:59 -0500

    Create ScannerClass.java

---
+++ JAVA-CODES/GUNDUZ/JavaNotes/ScannerClass.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class ScannerClass {
+    public static void main(String[] args) {
+
+
+    // SCANNER CLASS
+
+    //import java.util.Scanner; ==> between package and className
+    Scanner variableName = new Scanner(System.in);  //==> In main method
+        System.out.println("Enter a number: ");  //print something before enter value
+    int num =  variableName.nextInt(); // assign a variable and nextInt accept only int number in the scanner
+
+        System.out.println("Your number multiple by 5: " + num * 5); //result
+}
+}
