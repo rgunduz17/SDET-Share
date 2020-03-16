4747a9ee757873bf2a1df6459d233a8e8eab9107
RIDVAN GUNDUZ <58498991+rgunduz17@users.noreply.github.com>
2020-03-16 15:36:36 -0500

    Update StringClass.java

--- JAVA-CODES/JavaNotes/StringClass.java
+++ JAVA-CODES/JavaNotes/StringClass.java
@@ -68,6 +68,71 @@ public class StringClass {
 
         System.out.println("*****************************************");
 
+        // 6. trim()
+        // remove unused spaces
+
+            String a5= "       Hello";
+            a5 = a5.trim();
+
+        System.out.println("Spaces are trimmed "+a5);
+
+
+        System.out.println("*****************************************");
+
+        // 7. substring(beginning index)
+
+        String a6= "CyberTek School";
+
+        String a7= a6.substring(9);
+        System.out.println("Substring of "+ a6+ " is :  "+a7);
+
+//          substring (beginning index, last index)
+
+        String a8 = "Hello Cybertek School";
+        a8 = a8.substring(6,14);
+        System.out.println(a8);
+
+        System.out.println("*****************************************");
+
+        // 8. replace(old char, new char)
+
+
+
+        String a9 = "Java is Fun Programming Language";
+        // a9. replace('a', 'e');
+
+        a9= a9. replace('a', 'e');
+
+        System.out.println("Replaced all \"A\"s with \"E\"s : "  + a9);
+
+
+
+        //  replace (old str, new str)
+
+        String str3 = "Today is gonna be great day to learn JAVA";
+       str3 = str3.replace("Today", "Tomorrow");
+        System.out.println("Replaced \"Today\" with \"Tomorrow\": " + str3);
+
+        //  replaceFirst(old str, new str)
+
+        String str4 = "Java is fun, Java is good.";
+
+        str4 = str4.replaceFirst("Java", "Phyton");
+        System.out.println("Replaced first \"Java\" word with \"Phyton\" : "+str4);
+
+        //  replaceAll(old str, new str)
+
+
+        String str5 = "how to do in java provides java tutorials";
+
+          String newStr = str5.replaceAll("java", "Pyhton");
+
+          System.out.println("Replaced All \"java\" words with \"Phyton\": " + newStr);
+
+         System.out.println("*****************************************");
+
+
+
 
 
     }
