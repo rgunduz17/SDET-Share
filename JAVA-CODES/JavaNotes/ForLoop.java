9f28ccfbaaf84711c16d84b005ab759e2eaa16b9
RIDVAN GUNDUZ <58498991+rgunduz17@users.noreply.github.com>
2020-03-16 23:29:27 -0500

    Create ForLoop.java

---
+++ JAVA-CODES/JavaNotes/ForLoop.java
@@ -0,0 +1,9 @@
+package com.company;                         
+                                             
+                                             
+                                             
+public class ForLoop {                   
+    public static void main(String[] args) { 
+                                             
+}
+}
